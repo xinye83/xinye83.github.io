b54f01359d61ea0fbbc7b759fcd7e2f7d2040bec
Xin Ye <yexinwhu@gmail.com>
2019-08-22 00:00:56 -0500

    Update lang-fortran.js
    Still problem with comments.

--- google-code-prettify/lang-fortran.js
+++ google-code-prettify/lang-fortran.js
@@ -47,7 +47,7 @@ PR['registerLangHandler'](
         ],
         [
             // Fixed-form style comment - c or * in first column
-            [PR['PR_COMMENT'], /^\n+(?:c|\*).*/i],
+            [PR['PR_COMMENT'], /^\*[^\r\n]*/, null],
             // 'type(foo)' is a type
             // 'type foo' is a keyword
             [PR['PR_TYPE'], /^type *(?:\([^ \r\n]+\))/i],
